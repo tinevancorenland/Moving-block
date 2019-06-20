fda9a52fd98c51106f28a8a950c85f41baca4d79
“[tine <[tinevancorenland@gmail.com]>
2019-06-20 11:44:53 +0200

    make sure that the mover respects the borders of the container

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ const container = document.getElementById("container");
 const mover = document.getElementById("mover");
 
 //Make mover move when you press  keys
+//keyCodes of arrow keys are left = 37, right = 39, up = 38, down = 40
+
 document.onkeydown = animate;
 
 let moverLeft = 0;
 let moverTop = 0;
 
 function animate(e) {
-  //   console.log(e.keyCode); show me that arrow keys have following codes: left = 37, right = 39, up = 38, down = 40
   if (e.keyCode === 39) {
     moverLeft += 2;
+    if (moverLeft >= 475) {
+      moverLeft = 475;
+    }
   }
   if (e.keyCode === 37) {
     moverLeft -= 2;
+    if (moverLeft <= 0) {
+      moverLeft = 0;
+    }
   }
   if (e.keyCode === 40) {
     moverTop += 2;
+    if (moverTop >= 475) {
+      moverTop = 475;
+    }
   }
   if (e.keyCode === 38) {
     moverTop -= 2;
+    if (moverTop <= 0) {
+      moverTop = 0;
+    }
   }
-
   mover.style.left = moverLeft + "px";
   mover.style.top = moverTop + "px";
 }
